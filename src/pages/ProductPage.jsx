7f24772ba8298876bf7d5729cf4216a1805eda94
sayaoyuri <yuriantunes0000@gmail.com>
2023-08-12 17:23:29 -0300

    style (PRODUCT) styling product page

--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -2,9 +2,12 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import styled from "styled-components";
 import { getProduct } from "../services/services";
+import { FaWhatsapp } from 'react-icons/fa';
+import { AiOutlineMail } from 'react-icons/ai';
 
 const ProductPage = () => {
   const [product, setProduct] = useState({});
+  console.log(product)
   const { id } = useParams();
 
   useEffect(() => {
@@ -12,19 +15,22 @@ const ProductPage = () => {
   }, []);
 
   return (
-      <Container>
-      <section>
-        <h3>{product.category}</h3>
-        <h2>{product.name}</h2>
+    <Container>
+      <ProductDetails>
+        <h4>{product.category}</h4>
+        <h1>{product.name}</h1>
         <img src={product.imageUrl} alt="" />
-        <h1>{`R$ ${product.price}`}</h1>
-        <h5>{product.description}</h5>
-      </section>
-      <article>
+        <h2>{`R$ ${product.price}`}</h2>
+        <h3>{product.description}</h3>
+      </ProductDetails>
+      <OwnerDetails>
         <h1>{product.ownerName}</h1>
-        <h2>{product.email}</h2>
-        <h2>{`Whatsapp: ${product.phone}`}</h2>
-      </article>
+        <div>
+          <h2>Contatos</h2>
+          <h3><AiOutlineMail/>{` ${product.email}`}</h3>
+          <h3><FaWhatsapp/>{` ${product.phone}`}</h3>
+        </div>
+      </OwnerDetails>
     </Container>
   );
 };
@@ -34,28 +40,69 @@ export default ProductPage;
 const Container = styled.main`
   display: flex;
   gap: 10px;
-  width: 50%;
-  height: 50vh;
-  border: 1px solid black;
-  margin: 24vh auto;
+  width: 65%;
+  margin: 15vh auto 0 auto;
   border-radius: 5px;
-  padding: 5px;
+`;
 
-  section, article {
-    width: 60%;
-    height: 100%;
-    border: 1px solid black;
-    border-radius: 5px;
-    padding: 5px;
+const ProductDetails = styled.div`
+  width: 60%;
+  max-height: 75vh;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  box-shadow: rgba(0, 0, 0, 0.07) 0px 1px 2px, rgba(0, 0, 0, 0.07) 0px 2px 4px, rgba(0, 0, 0, 0.07) 0px 4px 8px, rgba(0, 0, 0, 0.07) 0px 8px 16px, rgba(0, 0, 0, 0.07) 0px 16px 32px, rgba(0, 0, 0, 0.07) 0px 32px 64px;
+  padding: 10px;
 
-    img {
-      margin: 0 auto;
-      width: 80%;
-      height: 40%;
-    };
+  h1 {
+    font-size: 22px;
   };
 
-  article {
-    width: 35%;
+  h2 {
+    font-size: 20px;
+  };
+
+  h3 {
+    font-size: 16px;
+  };
+
+  h4 {
+    font-size: 12px;
+  };
+  
+  img {
+    width: 90%;
+    height: 60%;
+  };
+`;
+
+const OwnerDetails = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 40%;
+  height: auto;
+  padding: 20px 5px;
+  box-shadow: rgba(0, 0, 0, 0.07) 0px 1px 2px, rgba(0, 0, 0, 0.07) 0px 2px 4px, rgba(0, 0, 0, 0.07) 0px 4px 8px, rgba(0, 0, 0, 0.07) 0px 8px 16px, rgba(0, 0, 0, 0.07) 0px 16px 32px, rgba(0, 0, 0, 0.07) 0px 32px 64px;
+
+  div {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    gap: 15px;
+    margin: 20% 0px ;
+  };
+
+  h1 {
+    font-size: 22px;
+  };
+
+  h2 {
+    font-size: 20px;
+    text-align: center;
+  };
+
+  h3 {
+    font-size: 18px;
   };
 `;
